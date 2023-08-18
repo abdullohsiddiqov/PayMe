447be5fddfa82c19f0eaad16bf35a708b5203b77
gang_dramma <your_email@kottaholakottahola@gmail.com>
2023-08-18 21:41:34 +0500

    add new files

--- src/main.js
+++ src/main.js
@@ -6,18 +6,24 @@ function InformationFNC() {
     btn.addEventListener('click', () => {
         if (password_input.value === "") {
             password_input.style.border = "1px solid red";
+            // number = null || number_input.value;
         }
         if (number_input.value === "") {
             number_input.style.border = "1px solid red";
+            // password = null || password_input.value;
         }
-        let password = password_input.value;
-        let number = number_input.value;
-        localStorage.setItem('password', password);
-        console.log(localStorage.getItem('password'));
-        localStorage.setItem('number', number);
-        console.log(localStorage.getItem('number'));
-        console.log(`password = ${password}`);
-        console.log(`number = ${number}`);
+        const data = {
+            password: password_input.value,
+            number: number_input.value,
+            console() {
+                console.log(this.password);
+                console.log(this.number);
+            }
+        };
+        console.log(data);
+        localStorage.setItem('data', JSON.stringify(data));
+        // console.log(`password = ${password}`);
+        // console.log(`number = ${number}`);
     });
 }
 InformationFNC();
--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,29 @@
 let number_input:HTMLInputElement = document.querySelector('.number_input')!;
 let btn:HTMLButtonElement = document.querySelector('.btn')!;
 let password_input:HTMLInputElement = document.querySelector('.password_input')!;
-
 function InformationFNC(){ 
     btn.addEventListener('click', () =>{ 
+
         if(password_input.value === ""){ 
             password_input.style.border = "1px solid red";
+            // number = null || number_input.value;
         }
         if(number_input.value === ""){ 
             number_input.style.border = "1px solid red";
+            // password = null || password_input.value;
         }
-        let password = password_input.value;
-        let number = number_input.value;
-        localStorage.setItem('password', password);
-        console.log(localStorage.getItem('password'));
-        localStorage.setItem('number', number);
-        console.log(localStorage.getItem('number'));
-        console.log(`password = ${password}`);
-        console.log(`number = ${number}`);
+        const data = {
+            password: password_input.value,
+            number: number_input.value,
+            console(){ 
+                console.log(this.password);
+                console.log(this.number);
+            }
+        };
+        console.log(data);
+        localStorage.setItem('data', JSON.stringify(data));
+        // console.log(`password = ${password}`);
+        // console.log(`number = ${number}`);
     });
 }
 InformationFNC()
