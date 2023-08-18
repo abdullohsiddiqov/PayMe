fb317864bb95fb28fe870c4cbc92d9d8b7a1f19c
gang_dramma <your_email@kottaholakottahola@gmail.com>
2023-08-18 21:17:55 +0500

    add local storage

--- src/main.js
+++ src/main.js
@@ -2,15 +2,22 @@
 let number_input = document.querySelector('.number_input');
 let btn = document.querySelector('.btn');
 let password_input = document.querySelector('.password_input');
-btn.addEventListener('click', () => {
-    if (password_input.innerHTML == "") {
-        password_input.style.border = "1px solid red";
-    }
-    if (number_input.innerHTML == "") {
-        number_input.style.border = "1px solid red";
-    }
-    let password = password_input.value;
-    let number = number_input.value;
-    console.log(password);
-    console.log(number);
-});
+function InformationFNC() {
+    btn.addEventListener('click', () => {
+        if (password_input.value === "") {
+            password_input.style.border = "1px solid red";
+        }
+        if (number_input.value === "") {
+            number_input.style.border = "1px solid red";
+        }
+        let password = password_input.value;
+        let number = number_input.value;
+        localStorage.setItem('password', password);
+        console.log(localStorage.getItem('password'));
+        localStorage.setItem('number', number);
+        console.log(localStorage.getItem('number'));
+        console.log(`password = ${password}`);
+        console.log(`number = ${number}`);
+    });
+}
+InformationFNC();
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,22 @@ let number_input:HTMLInputElement = document.querySelector('.number_input')!;
 let btn:HTMLButtonElement = document.querySelector('.btn')!;
 let password_input:HTMLInputElement = document.querySelector('.password_input')!;
 
-btn.addEventListener('click', () =>{ 
-    if(password_input.innerHTML == ""){ 
-        password_input.style.border = "1px solid red";
-    }
-    if(number_input.innerHTML == ""){ 
-        number_input.style.border = "1px solid red";
-    }
-    let password = password_input.value;
-    let number = number_input.value;
-    console.log(password);
-    console.log(number);
-});
+function InformationFNC(){ 
+    btn.addEventListener('click', () =>{ 
+        if(password_input.value === ""){ 
+            password_input.style.border = "1px solid red";
+        }
+        if(number_input.value === ""){ 
+            number_input.style.border = "1px solid red";
+        }
+        let password = password_input.value;
+        let number = number_input.value;
+        localStorage.setItem('password', password);
+        console.log(localStorage.getItem('password'));
+        localStorage.setItem('number', number);
+        console.log(localStorage.getItem('number'));
+        console.log(`password = ${password}`);
+        console.log(`number = ${number}`);
+    });
+}
+InformationFNC()
