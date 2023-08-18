8282af647b9fde0ae077bc03bed594bfb01d065a
gang_dramma <your_email@kottaholakottahola@gmail.com>
2023-08-18 18:22:39 +0500

    finish the register

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
 "use strict";
 let number_input = document.querySelector('.number_input');
+let btn = document.querySelector('.btn');
+let password_input = document.querySelector('.password_input');
+btn.addEventListener('click', () => {
+    if (password_input.innerHTML == "") {
+        password_input.style.border = "1px solid red";
+    }
+    if (number_input.innerHTML == "") {
+        number_input.style.border = "1px solid red";
+    }
+    let password = password_input.value;
+    let number = number_input.value;
+    console.log(password);
+    console.log(number);
+});
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,17 @@
 
 let number_input:HTMLInputElement = document.querySelector('.number_input')!;
+let btn:HTMLButtonElement = document.querySelector('.btn')!;
+let password_input:HTMLInputElement = document.querySelector('.password_input')!;
+
+btn.addEventListener('click', () =>{ 
+    if(password_input.innerHTML == ""){ 
+        password_input.style.border = "1px solid red";
+    }
+    if(number_input.innerHTML == ""){ 
+        number_input.style.border = "1px solid red";
+    }
+    let password = password_input.value;
+    let number = number_input.value;
+    console.log(password);
+    console.log(number);
+});
